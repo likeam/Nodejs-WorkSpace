f5002d44641003f20774abe01685f74afbbccd12
abdul <likeam99@gmail.com>
2024-11-03 13:07:39 +0500

    ES6 Import and export

---
+++ ES6 Imports/index.js
@@ -0,0 +1,12 @@
+import greet from "./greet.js";
+import { p1, p2, p3 } from "./people.js";
+
+greet("HaniaB");
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+
+greet(p1);
+greet(p2);
+greet(p3);
---
+++ ES6 Imports/people.js
@@ -0,0 +1,5 @@
+let p1 = "zahid Wasim";
+let p2 = "Imran Zia";
+let p3 = "Abdul Rehman";
+
+export { p1, p2, p3 };
