6d777853254e4575e24803e9e020671ff534b117
abdul <likeam99@gmail.com>
2024-11-04 00:36:37 +0500

    Building Path module

--- PathModule/index.js
+++ PathModule/index.js
@@ -1,3 +1,14 @@
 import path from "path";
 
-console.log(path.basename("c:\\nodejs\\index.js"));
+console.log(path.basename("c:\\nodejs\\index.js", ".js"));
+
+console.log(path.dirname("c:\\comments\\vote\\index.js"));
+console.log(path.extname("c:\\nodejs\\index.js"));
+
+console.log(path.join("c:", "hania", "mavia", "rehman"));
+
+console.log(path.join("c:", "hania", "mavia", "rehman", ".."));
+
+console.log(path.join("c:", "hania", "mavia", "rehman", "..", ".."));
+
+console.log(path.normalize("c:\\desk\\\\stor"));
